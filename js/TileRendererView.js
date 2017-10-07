172bfd291658eec2a344d75a4066578b60d56846
Ugo Parlange <uparlange@gmail.com>
2017-10-07 09:44:07 +0200

    Add combo options

--- js/TileRendererView.js
+++ js/TileRendererView.js
@@ -4,13 +4,23 @@ define(["AppUtils", "AbstractRendererComponent", "RendererHelper"],
 			extends: AbstractRendererComponent,
 			constructor: function TileRendererView(RendererHelper) {
 				AbstractRendererComponent.call(this, RendererHelper);
+				this.selected = null;
 			},
 			parameters: [
 				[RendererHelper]
 			],
 			annotations: [
 				new ng.core.Component(AppUtils.getComponentConfiguration("tile-renderer-view"))
-			]
+			],
+			functions: {
+				getComboPosition: function (index) {
+					return ((index % 4) !== 0);
+				},
+				onAction: function (game, option) {
+					this.helper.executeGameOption(game, option);
+					this.selected = {};
+				}
+			}
 		});
 	}
 );
